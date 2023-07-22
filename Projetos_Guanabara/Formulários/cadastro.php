e8ee0d7b598dfd61d7f51ac4459a5529b3d013c7
SamuelCambui <106870806+SamuelCambui@users.noreply.github.com>
2023-07-22 20:14:53 -0300

    atualizaçõs

--- Projetos_Guanabara/Formulários/cadastro.php
+++ Projetos_Guanabara/Formulários/cadastro.php
@@ -1,6 +1,38 @@
 <?php
 
-echo $_POST ["usu"] ."<br>";
-echo $_POST ["sen"];
+$user = $_POST ["usu"] ."<br>";
+$senha =  $_POST ["sen"];
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "escola";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO escola (al_user, al_password)
+VALUES ($user,$senha)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
 
 ?>
+
+
+
+
+<script>
+alert('Cadastrado com sucesso');
+location.href = "index.html";
+</script>
