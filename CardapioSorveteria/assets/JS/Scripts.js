28da06d4726bf8cde09b7fe59b2b2fe25c6853b0
SamuelCambui <106870806+SamuelCambui@users.noreply.github.com>
2023-07-29 18:04:11 -0300

    abrir insta e whats

--- CardapioSorveteria/assets/JS/Scripts.js
+++ CardapioSorveteria/assets/JS/Scripts.js
@@ -1,3 +1,7 @@
-document.getElementById("Insta").addEventListener("click", function() {
+function abrirLinkInsta() {
     window.open("https://instagram.com/sorveteourobranco?igshid=MzRlODBiNWFlZA==", "_blank");
-  });
+  }
+
+  function abrirLinkWhats(){
+      window.open("https://api.whatsapp.com/send?phone=555534998070916&text=Oii%21+Meu+nome+%C3%A9&fbclid=PAAaZmfpoke4pHk_nvnnNJ4hzzb3UpnVPlgE3I0GX0FupOTyefUHyJF5Mxxl4","_blank");
+  }
