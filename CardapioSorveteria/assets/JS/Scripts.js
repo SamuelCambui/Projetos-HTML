2ef47e5dce4d624399cf1f7fef46294460bf6f6c
SamuelCambui <106870806+SamuelCambui@users.noreply.github.com>
2023-07-30 16:26:48 -0300

    telacomverde

--- CardapioSorveteria/assets/JS/Scripts.js
+++
@@ -1,7 +0,0 @@
-function abrirLinkInsta() {
-    window.open("https://instagram.com/sorveteourobranco?igshid=MzRlODBiNWFlZA==", "_blank");
-  }
-
-  function abrirLinkWhats(){
-      window.open("https://api.whatsapp.com/send?phone=555534998070916&text=Oii%21+Meu+nome+%C3%A9&fbclid=PAAaZmfpoke4pHk_nvnnNJ4hzzb3UpnVPlgE3I0GX0FupOTyefUHyJF5Mxxl4","_blank");
-  }
